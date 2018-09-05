20d9496486421e610b19bf23a8e5b272b13c4998
dval <dylan.valentine@gmail.com>
2018-09-05 12:04:52 -0400

    Added despawn event

--- Assets/Scripts/EnemyDespawner.cs
+++ Assets/Scripts/EnemyDespawner.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyDespawner : MonoBehaviour {
+
+	public UnityEvent OnDespawn;
 
 	void OnCollisionEnter(Collision bump){
 		if (bump.gameObject.tag == "Enemy" || bump.gameObject.tag == "Projectile") {
 			Destroy (bump.gameObject);
+
+			OnDespawn.Invoke ();
 		}
 
 	}
